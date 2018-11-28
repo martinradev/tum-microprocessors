40c6854c6274891f3ac044564c4d66cf3e1bcfdd
Martin Radev <martin.b.radev@gmail.com>
2018-11-28 14:48:14 +0100

    Add script to generate results. Start report in latex

---
+++ ex1/generate_results.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+MAX_THREADS=$1
+
+exeprog()
+{
+	progname=$1
+	startsz=10000
+	endsz=80000
+	stepsz=10000
+	i=0
+	while [ "$i" -lt "$MAX_THREADS" ]; do
+		f="${progname}_${startsz}_${endsz}_${i}.txt"
+		cmd="./$progname -l $startsz $endsz $stepsz -t $i > $f"
+		echo "Running $cmd"
+		eval $cmd
+		i=$((i+1))
+	done
+}
+
+for u in 1 2 3 4; do
+	exeprog "toupper_variant$u"
+done
+
+
